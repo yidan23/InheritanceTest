e371addbd995a20adb3968c684328e5733f31087
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-02-08 12:07:26 -0500

    finished

--- Tester.java
+++ Tester.java
@@ -1,57 +1,67 @@
+//Test of Inheritance
+//import ArrayList
+import java.util.ArrayList;
+//PERSON CLASS that is a parent of Scholar and Instructor
 class Person{
 	public String name;
 	public int yob;
+	//constructor that sets Person variables
 	public Person(String n, int y){
 		name = n;
 		yob = y;
 	}
-	 public String toString(){
-		return ("Persons name: " + name + "\n  yob:" + yob);
-	 }
+	//converts data to formatted string 
+	public String toString(){
+		return ("PERSONS NAME: " + name + "\n  - Year of birth: " + yob);
+	}
 }
-//no constructors allowed
+//SCHOLAR CLASS that is a child of Person
 class Scholar extends Person{
 	public String major;
-	
-	 public Scholar(String name, int birth, String major){
-		 super(name, birth);
-		 this.major = major;
-	 }
-	 
-	 public String toString(){
-		return ("Scholars name: " + super.name + "\n  major:" + major + "\n  yob:" + super.yob); 
-	 }
+	//constructor that uses Person variables and major
+	public Scholar(String name, int birth, String major){
+		super(name, birth);
+		this.major = major;
+	}
+	 //converts data to formatted string 
+	public String toString(){
+		return ("SCHOLARS NAME: " + super.name + "\n  - Year of birth: " + super.yob + "\n  - Major: " + major); 
+	}
 }
 
+//INSTRUCTOR CLASS that is a child of Person
 class Instructor extends Person{
 	public int salary;
-	
+	//constructor that uses Person variables and salary
 	public Instructor(String name, int birth, int salary){
 		 super(name, birth);
 		 this.salary = salary;
-	 }
-	 
-	 public String toString(){
-		return ("Instructor name: " + super.name + "\n  salary: $" + salary+ "\n  yob:" + super.yob);
-	 }
+	}
+	//toString method to format data
+	public String toString(){
+		return ("INSTRUCTORS NAME: " + super.name  + "\n - Year of birth: " + super.yob + "\n - Salary: $" + salary);
+	}
 }
-
+//TESTER CLASS: This calls instances of the other classes and puts those objects into an ArrayList and prints them out
 public class Tester{
+	//main function
 	public static void main(String args[]){
 		Person p1 = new Person("Bob", 1939);
 		Person p2 = new Person("Joe", 1972);
 		
-		Scholar s1 = new Scholar("Ryan", 1999,"Math");
-		Scholar s2 = new Scholar("Kai", 1991,"English");
+		Scholar s1 = new Scholar("Ryan", 2005, "Math");
+		Scholar s2 = new Scholar("Kai", 1991, "English");
 		
-		Instructor i1 = new Instructor("Jeff", 2999, 12300);
-		Instructor i2 = new Instructor("Greg", 1789, 32100);
+		Instructor i1 = new Instructor("Jeff", 1955, 90300);
+		Instructor i2 = new Instructor("Greg", 2000, 52100);
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(s1);
-		System.out.println(s2);
-		System.out.println(i1);
-		System.out.println(i2);
+		ArrayList<Person> arr = new ArrayList<Person>();
+		
+		arr.add(p1); arr.add(p2); arr.add(s1); arr.add(s2); arr.add(i1); arr.add(i2); 
+		
+		for(int i = 0; i<arr.size(); i++){
+			System.out.println(arr.get(i));
+		}
 	}
 }
+
